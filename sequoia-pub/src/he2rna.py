79f3b81076cec6db59a90cc09ccf21106920d1ec
Siddarth <schiluku@student.ubc.ca>
2026-03-05 12:53:17 -0800

    my bad again

--- sequoia-pub/src/he2rna.py
+++ sequoia-pub/src/he2rna.py
@@ -13,7 +13,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from tkinter.messagebox import NO
 import numpy as np
 import pandas as pd
 import torch
